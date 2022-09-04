4d1275ed40a9f33e2d5e2e04e71222f810eecfab
Deivid Michael <deividmichael2018@gmail.com>
2022-09-03 23:31:16 -0300

    commit 1

--- index.js
+++ index.js
@@ -1,54 +1,23 @@
 let B7validator = {
-    handleSubmit:(Event)=>{
-        Event.preventDefault();
-        let send = true;
+    handleSubmit:(event)=>{
+    event.preventDefault();
+    let send = true;
 
-        let inputs = form.querySelectorAll('input');
+    let inputs = form.querySelectorAll('input');
 
-        for(let i=0;i<inputs.length;i++) {
-            let input = inputs[i];
-            let check = B7validator.checkInput(input);
-            if(check !== true) {
-                send = false;
-                B7validator.showError(input, check);
-            }
-        }
-  
-        if(send) {
-            form.submit();
-        }
-    },
-    checkInput:(input) => {
-        let rules = input.getAttribute('data-rules');
-        
-        if(rules !== null) {
-             rules = rules.split('|');
-             for(let k in rules) {
-                let rDetaisls = rules[k].split('=');
-                switch(rDetaisls[0]) {
-                    case 'required':
-                         if(input.value == '') {
-                            return 'Campo não pode ser vazio.';
-                         }
-                    break;
-                    case 'min':
-
-                    break;
-
-                }
-             }
-        }
-
-        return true;
-    },
-    showError:(input, console.error) => {
-       input.style.borderColor = '#FF0000';
-       
-       let errorElement = document.createElement('div');
-       errorElement.classList.add('error');
-       errorElement.innerHTML = error; 
+    for(let i=0;i<inputs.length;i++) {
+        let input = inputs[i];
+         console.log(input);
     }
-},
+    
+    send = false;
+    if(send) {
+        form.submit();
+       
+      }
+    }
+};
+
 
 let form = document.querySelector('.b7validator');
-form.addEventListener('submit', b7validator.handleSubmit);
+form.addEventListener('.submit',)
